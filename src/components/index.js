e154e4a576b05e668b4f3d18af5b2657c822ae4f
Pouya <titanpooyanote@gmail.com>
2024-09-29 12:11:28 +0330

    images added

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ export {QuickAccess} from './landing/QuickAccess'
 export {WeekPop} from './landing/WeekPop'
 export {WeekTeacher} from './landing/WeekTeacher'
 export {Button} from './common/Button'
-export {Telegram} from '../assets/logo/telegram-stroke-rounded 1.svg?react'
-export {Instagram} from '../assets/logo/instagram-stroke-rounded 1.svg?react'
-export {Logo} from '../assets/logo/Untitled-1 2.svg?react'
-export {eNamad} from '../assets/logo/images.png'
+export { ReactComponent as Telegram } from '../assets/logo/telegram-stroke-rounded 1.svg';
+export { ReactComponent as Instagram } from '../assets/logo/instagram-stroke-rounded 1.svg';
+export { ReactComponent as Logo } from '../assets/logo/Untitled-1 2.svg';
+export { default as eNamad } from '../assets/logo/images.png'; // For image imports
