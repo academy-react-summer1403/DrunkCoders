33a08a08cd629f1ac2b7ca6f1f3e6fab806bbcaa
Pouya <titanpooyanote@gmail.com>
2024-10-01 02:09:54 +0330

    to merge footer

--- src/components/index.js
+++ src/components/index.js
@@ -5,11 +5,8 @@ export {QuickAccess} from './landing/QuickAccess'
 export {WeekPop} from './landing/WeekPop'
 export {WeekTeacher} from './landing/WeekTeacher'
 export {Button} from './common/Button'
-<<<<<<< HEAD
 export {BaseInput} from './common/BaseInput'
-=======
 export { default as Telegram } from '../assets/logo/telegram-stroke-rounded 1.svg?react';
 export { default as Instagram } from '../assets/logo/instagram-stroke-rounded 1.svg?react';
 export {default as Logo } from '../assets/logo/Untitled-1 2.svg?react';
 export { default as eNamad } from '../assets/logo/images.png';
->>>>>>> footer
