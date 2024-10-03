aec154e3dac630eb0e2b84bb8740747e48dea548
Mojtaba-Gholizadeh <mojtaba.leoooo@gmail.com>
2024-10-03 13:37:56 +0330

    some changes made so that we can use svg image more than one time.

--- src/pages/auth/AuthLayout.jsx
+++ src/pages/auth/AuthLayout.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
-import ShortLogo from '../../assets/logo/short-Logo.svg?react';
-import LongLogo from '../../assets/logo/long-Logo.svg?react';
+import ShortLogo from '../../assets/logo/short-Logo.svg?react'
+// import ShortLogo2 from '../../assets/logo/short-Logo.svg?react'
+import LongLogo from '../../assets/logo/long-Logo.svg?react'
+import {useEffect, useState} from 'react'
 
-export function AuthLayout({ children, sideBar }) {
-  return (
-    <main className="flex md:flex-row flex-col">
-      <aside className="flex flex-col md:w-6/12 md:bg-[#E4E4E4] md:h-screen order-1 md:order-none ">
-        <div className="flex flex-col md:mt-[25%] md:mr-[10%] m-auto w-fit mt-12 pb-16">
-            <div className='md:flex items-center hidden'>
-                <ShortLogo className="w-[58px] h-[55px]" />
-                <LongLogo className="w-[189px] h-[38px]" />
+export function AuthLayout({children, sideBar}) {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth < 768 ? null : 'md')
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            const width = window.innerWidth
+            if (width < 768) {
+                setWindowWidth(null)
+            } else {
+                setWindowWidth('md')
+            }
+        })
+    }, [])
+    return (
+        <main className="flex md:flex-row flex-col">
+            <aside className="hidden md:flex flex-col md:w-6/12  md:bg-[#E4E4E4] md:h-screen order-1 md:order-none ">
+                <div className="flex flex-col md:mt-[25%] md:mr-[10%] m-auto w-fit mt-12 pb-16">
+                    <div className="md:flex items-center hidden">
+                        {windowWidth === 'md' && <ShortLogo className="w-[58px] h-[55px]" />}
+                        <LongLogo className="w-[189px] h-[38px]" />
+                    </div>
+
+                    {sideBar && (
+                        <div className="md:mt-8 ml-8  flex flex-col gap-8 text-sm text-gray-500">
+                            {sideBar}
+                        </div>
+                    )}
+                </div>
+            </aside>
+
+            <div className="w-full md:h-screen flex justify-center">
+                <div className="w-fit mt-[15%] mx-3">
+                    <div className=" md:hidden">
+                        {console.log('Rendering ShortLogo')}
+                        {/* problem is here */}
+                        {windowWidth === null && <ShortLogo />}
+                    </div>
+                    {children}
+                </div>
             </div>
-            
-            {sideBar && <div className="md:mt-8 ml-8  flex flex-col gap-8 text-sm text-gray-500">{sideBar}</div>}
-        </div>      
-      </aside>
-
-      <div className="w-full md:h-screen flex justify-center">
-        <div className="w-fit mt-[15%] mx-3">
-          <div className='block md:hidden'>
-          {console.log('Rendering ShortLogo')}
-          {/* problem is here */}
-          <ShortLogo/>
-          </div>     
-          {children}
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    )
 }
