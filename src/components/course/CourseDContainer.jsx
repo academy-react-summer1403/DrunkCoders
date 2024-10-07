b56753604c8176123a8d205ac02f981207b85a01
Pouya <titanpooyanote@gmail.com>
2024-10-07 21:28:54 +0330

    prepare to merge

--- src/components/course/CourseDContainer.jsx
+++ src/components/course/CourseDContainer.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Button } from '@components'
 
 export function CourseDContainer() {
   return (
     <>
-    <div className='mt-16 flex border-3 rounded-3xl h-screen gap-[10%]'>
-        <div className='w-[40%] border-3 rounded-3xl p-3 h-fit'>
-            <h1>ری‌اکت جی‌اس</h1>
-            
+    <div className='mt-4 border-3 rounded-3xl h-screen gap-[10%]'>
+      <div className='mt-12 flex'>
+        
+        <div className='flex flex-col w-[40%] border-3 rounded-3xl p-3 h-fit'>
+            <div className='w-fit flex items-center gap-2 rounded-xl bg-[#FFD1CB] px-2 py-1 text-xs text-[#FF5454]'>
+              <div className='h-2 w-2 rounded-full bg-[#FF5454]'></div>
+              <span> درحال برگزاری </span>
+            </div>
+            <h1 className='text-2xl font-bold mt-5'>ری‌اکت جی‌اس</h1>
+            <div className='flex gap-2 w-fit'>
+            <Button className='font-xs' >برنامه نویسی</Button>
+            <Button>مبتدی</Button>
+            </div>
         </div>
         <div className='w-[50%] '></div>
+      </div>
     </div>
     </>
   )
