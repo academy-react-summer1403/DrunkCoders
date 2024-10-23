4447c5548b1fea2d29634ff1a7b7d469963eecd9
Pouya <titanpooyanote@gmail.com>
2024-10-23 16:08:08 +0330

    refactor comment design

--- src/components/articleDetails/articleComments/ArticleCommentList.jsx
+++ src/components/articleDetails/articleComments/ArticleCommentList.jsx
@@ -1,7 +1,5 @@
 // CommentList.jsx
-import { CommentArrow, ThumbDown, ThumbUp } from '@assets/index';
-import { Button } from '@components/index';
-import { Avatar } from '@nextui-org/react';
+import { DesignComment } from '@components/courseDetails/courseComment/DesignComment';
 import React from 'react';
 
 export function ArticleCommentList({ comments }) {
@@ -9,46 +7,16 @@ export function ArticleCommentList({ comments }) {
     <div className="comment-list">
       {comments.map((comment) => (
         <div key={comment.id} className="comment">
-           <div className={` rounded-3xl rounded-t-none rounded-l-none border-gray-400`}>
-        <div className="flex items-center gap-2 -mr-7">
-          <Avatar src={comment.pictureAddress} size="lg" />
-          <span>{comment.autor}</span>
-        </div>
-        <div className="mr-8 mt-3 pb-12">
-          <p><strong>{comment.title}</strong></p>
-          <p className="mt-2">{comment.describe}</p>
-          <div className='flex items-center gap-16'>
-            <Button
-              // onPress={() => handleOpenModal(true, comment, true)}
-              className="-mr-1 mt-4 h-9 w-32 bg-blue-200 font-medium text-blue-600"
-            >
-              <CommentArrow />
-              جواب دادن
-            </Button>
-            <div className='flex gap-8 items-center mt-4 z-50'>
-              <div className='flex gap-1 items-center' 
-              // onClick={() => handleLike("like")}
-              >
-                <ThumbUp
-                  className={`-mt-1 stroke-black dark:stroke-white hover:text-primary-blue
-                    *\${likeState.like ? "text-primary-blue stroke-primary-blue" : "text-transparent"}`}
-                />
-                {comment.likeCount}
-              </div>
-              <div className='flex gap-1 items-center'
-              // onClick={() => handleLike("dislike")}
-              >
-                <ThumbDown
-                  className={`stroke-black dark:stroke-white hover:text-primary-blue
-                    *\${likeState.dislike ? "text-primary-blue stroke-primary-blue" : "text-transparent"}`}
-                />
-                {comment.dissLikeCount}
-              </div>
-            </div>
-            
-          </div>
-        </div>
-      </div>
+            <DesignComment
+                finalReplies
+                likeState
+                pictureAddress={comment.pictureAddress}
+                author={comment.autor}
+                describe={comment.describe}
+                comment={comment}
+                likeCount={comment.likeCount}
+                dislikeCount={comment.disslike}
+            />
         </div>
       ))}
     </div>
--- src/components/courseDetails/courseComment/DesignComment.jsx
+++ src/components/courseDetails/courseComment/DesignComment.jsx
@@ -1,9 +1,78 @@
+import { CommentArrow, ThumbDown, ThumbUp } from '@assets/index'
+import { Accordion, AccordionItem, Avatar } from '@nextui-org/react'
+import { Button } from '@components/index';
 import React from 'react'
 
-export function DesignComment() {
+export function DesignComment({finalReplies,pictureAddress,
+  author,title,describe,likeCount,dislikeCount,comment,handleOpenModal,
+  handleLike,likeState}) {
   return (
-    <div>
-      
+    <div className="comment flex flex-col p-7">
+      <div className={`${finalReplies.length > 0 ? 'border-r-3 border-b-3' : ''} rounded-3xl rounded-t-none rounded-l-none border-gray-400`}>
+        <div className="flex items-center gap-2 -mr-7">
+          <Avatar src={pictureAddress} size="lg" />
+          <span>{author}</span>
+        </div>
+        <div className="mr-8 mt-3 pb-12">
+          <p><strong>{title}</strong></p>
+          <p className="mt-2">{describe}</p>
+          <div className='flex items-center gap-16'>
+            <Button
+              onPress={() => handleOpenModal(true, comment, true)}
+              className="-mr-1 mt-4 h-9 w-32 bg-blue-200 font-medium text-blue-600"
+            >
+              <CommentArrow />
+              جواب دادن
+            </Button>
+            <div className='flex gap-8 items-center mt-4 z-50'>
+              <div className='flex gap-1 items-center' 
+              onClick={() => handleLike("like")}>
+                <ThumbUp
+                  className={`-mt-1 stroke-black dark:stroke-white hover:text-primary-blue
+                    ${likeState.like ? "text-primary-blue stroke-primary-blue" : "text-transparent"}`}
+                />
+                {likeCount}
+              </div>
+              <div className='flex gap-1 items-center'
+              onClick={() => handleLike("dislike")}>
+                <ThumbDown
+                  className={`stroke-black dark:stroke-white hover:text-primary-blue
+                    ${likeState.dislike ? "text-primary-blue stroke-primary-blue" : "text-transparent"}`}
+                />
+                {dislikeCount}
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+      {finalReplies.length > 0 && (
+        <Accordion isCompact
+        className='p-0'
+        >
+          <AccordionItem title="مشاهده جواب‌ها"
+              classNames ={{
+                heading : 'w-fit lg:-mt-24 lg:mr-96 -mt-10 mr-10',
+              }}
+          >
+            <div className="replies mr-6 ">
+              {finalReplies.map((reply) => (
+                <div key={reply.id} className="reply bg-white mt-5 mr-7 dark:bg-black">
+                  <div className="flex items-center gap-2">
+                    <Avatar src={reply.pictureAddress} size="lg" />
+                    <span>{reply.author}</span>
+                  </div>
+                  <div className="mr-8">
+                    <p><strong>{reply.title}</strong></p>
+                    <p>{reply.describe}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </AccordionItem>
+        </Accordion>
+      )}
     </div>
   )
 }
