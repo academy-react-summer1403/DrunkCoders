b1e880c55d52e3126dddbf8dcf3eb3074fd2d29e
Mojtaba-Gholizadeh <mojtaba.leoooo@gmail.com>
2024-10-28 15:54:03 +0330

    some other bug fixed

--- src/components/common/card/LikeAndDislike.jsx
+++ src/components/common/card/LikeAndDislike.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react'
 import { ThumbUp, ThumbDown } from '@assets'
-import { addLikeForCourse } from '@core/index'
 
 export function LikeAndDislike({
   like,
