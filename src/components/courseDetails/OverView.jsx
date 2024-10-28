4b0b0018827c6246879bfe1bb4f5441b38e58b34
Pouya <titanpooyanote@gmail.com>
2024-10-28 22:11:11 +0330

    comment post enable

--- src/components/courseDetails/OverView.jsx
+++ src/components/courseDetails/OverView.jsx
@@ -6,7 +6,7 @@ import { reserveCourse,addCourseFavorite } from '@core';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useDisclosure } from '@nextui-org/react';
 import { ReserveModal } from './ReserveModal';
-import { removeCourseFavorite } from '@core/index';
+import { addDislikeForCourse, addLikeForCourse, removeCourseFavorite, removeCourseLikeOrDislike } from '@core/index';
 
 export function OverView({ course }) {
   const [likeState, setLikeState] = useState({ like: false, dislike: false });
@@ -14,6 +14,7 @@ export function OverView({ course }) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const courseId = course.courseId; 
   const queryClient = useQueryClient();
+  const userLikeId = course.userLikeId;
 
   const farsiDateFormatter = new Intl.DateTimeFormat('fa-IR', {
     year: 'numeric',
@@ -60,7 +61,6 @@ export function OverView({ course }) {
     const payload = {
       favoriteId: course.userFavoriteId
     }
-    console.log(payload);
     const formData = new FormData();
     formData.append('CourseFavoriteId', payload.favoriteId);
 
@@ -84,13 +84,56 @@ export function OverView({ course }) {
     });
   }
   
+  const { mutate: addCourseLike } = useMutation( {
+    mutationFn: (courseId) => addLikeForCourse(courseId),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['courseDetails']);
+    }
+  })
+
+  const { mutate: addCourseDislike } = useMutation({
+    mutationFn: (courseId) => addDislikeForCourse(courseId),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['courseDetails']);
+    }
+  })
+
+  const removeLikeDislike = useMutation({
+    mutationFn: removeCourseLikeOrDislike,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['courseDetails']);
+    },
+    onError: (error) => {
+      console.log('Failed to remove like/dislike', error);
+    },
+  });
+  
+  function handleRemoveLikeDislike(userLikeId) {
+    const formData = new FormData();
+    formData.append('CourseLikeId', userLikeId);
+  
+    removeLikeDislike.mutate({ courseIdFormData: formData });
+  }
+  
 
   function handleLike(identifier) {
-    setLikeState((prevState) =>
-      identifier === "like"
-        ? { like: !prevState.like, dislike: false }
-        : { dislike: !prevState.dislike, like: false },
-    );
+    if (identifier === "like") {
+      if (likeState.like) {
+        handleRemoveLikeDislike(userLikeId);
+      } else {
+        addCourseLike(courseId);
+        if (likeState.dislike) handleRemoveLikeDislike(courseId);
+      }
+      setLikeState({ like: !likeState.like, dislike: false });
+    } else if (identifier === "dislike") {
+      if (likeState.dislike) {
+        handleRemoveLikeDislike(courseId);
+      } else {
+        addCourseDislike(courseId);
+        if (likeState.like) handleRemoveLikeDislike(courseId);
+      }
+      setLikeState({ like: false, dislike: !likeState.dislike });
+    }
   }
 
   return (
@@ -148,7 +191,7 @@ export function OverView({ course }) {
         <div className="rounded-full p-2 border-2 cursor-pointer" onClick={() => handleBookmark()}>
           <Bookmark
             className={`stroke-black dark:stroke-white hover:text-primary-blue
-            ${isBookmarked ? "text-primary-blue stroke-primary-blue" : "text-transparent"}`}
+            ${isBookmarked ? "text-primary-blue" : "text-transparent"}`}
           />
         </div>
 
@@ -156,7 +199,7 @@ export function OverView({ course }) {
           <div className="rounded-full p-2 border-2 cursor-pointer" onClick={() => handleLike("like")}>
             <ThumbUp
               className={`-mt-1 stroke-black dark:stroke-white hover:text-primary-blue
-              ${likeState.like ? "text-primary-blue stroke-primary-blue" : "text-transparent"}`}
+              ${likeState.like ? "text-primary-blue" : "text-transparent"}`}
             />
           </div>
           {course.likeCount}
